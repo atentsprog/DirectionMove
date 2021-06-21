5a05c6c16e021377aa3e61c606443781b3c7eda2
hktrollgames <hktrollgames@gmail.com>
2021-06-21 19:26:22 +0900

    애니메이션 길이 구현하기전

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -10,22 +10,54 @@ public class DirectionMove : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         lastMoveDirection = transform.forward;
+
+        //aimationLength 초기화.
     }
 
-    // Update is called once per frame
+    Dictionary<string, float> aimationLength = null;
+
     void Update()
     {
         Vector3 move = Vector3.zero; // move = new Vector3(0, 0, 0)
+
+        // 어택을 했으면 어택 애니메이션 진행중인 동안은 
+        // Run과 Idle을 하지 않게 하자
+        // 각 애니메이션의 길이가 필요하다.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //어택에니메이션이 끝났을때 state = None
+            StartCoroutine(AttackCo());
+        }
+
+        if (state != StateType.Attack)
+        {
+            move = MoveAndIdle(move);
+        }
+
+        transform.forward = Vector3.Slerp(transform.forward
+            , move, roateLerp);
+    }
+
+    IEnumerator AttackCo()
+    {
+        state = StateType.Attack;
+        animator.Play("Attack");
+        float attackAnimationTime = aimationLength["Attack"];
+        yield return new WaitForSeconds(attackAnimationTime);
+        state = StateType.None;
+    }
+
+    /// <summary>
+    /// Move/Idle애니메이션, 실제 이동
+    /// </summary>
+    /// <param name="move"></param>
+    /// <returns></returns>
+    Vector3 MoveAndIdle(Vector3 move)
+    {
         if (Input.GetKey(KeyCode.A)) move.x = -1;
         if (Input.GetKey(KeyCode.D)) move.x = 1;
         if (Input.GetKey(KeyCode.W)) move.z = 1;
         if (Input.GetKey(KeyCode.S)) move.z = -1;
-
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            state = StateType.Attack;
-            animator.Play("Attack", 1);
-        }
 
         if (move != Vector3.zero)
         {
@@ -43,23 +75,8 @@ public class DirectionMove : MonoBehaviour
             move = lastMoveDirection;
             state = StateType.Idle;
             animator.Play("Idle");
-        }
-
-        transform.forward = Vector3.Slerp(transform.forward
-            , move, roateLerp);
-
-        //transform.forward = transform.forward; // roateLerp ==0; 오른쪽,
-        //transform.forward = move; // roateLerp == 1;
-
-        //roateLerp: 0.05
-        //transform.forward : 앞쪽,100
-        //move; 오른쪽(0)
-        //    // 1frame : 95
-        //    // 2frame : 91
-        //    // 3frame : 87
-        //    // 4frame : 84
-        //    // 5frame : 83
-        //    // 60 frame : 0
+        };
+        return move;
     }
     public float speed = 5;
     public float roateLerp = 0.5f;
@@ -68,6 +85,10 @@ public class DirectionMove : MonoBehaviour
     public StateType state = StateType.Idle;
     public enum StateType
     {
-        Idle, Run, Jump, Attack,
+        None,  // 아무것도 안하는 상태, 어택이 끝나면 되는 상태
+        Idle,  // 가만있을때,...
+        Run, 
+        Jump, 
+        Attack,
     }
 }
